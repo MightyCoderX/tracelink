623eed604a71c9ec9f4dd94d657f9a473b8e32ae
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-11-23 16:24:34 +0100

    fix: enoent when symlink target is relative

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #define _POSIX_C_SOURCE 200112L
 #define _XOPEN_SOURCE 500
 
-#include <asm-generic/errno-base.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,6 +8,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <libgen.h>
 
 int main(int argc, char** argv) {
     if(argc != 2) {
@@ -16,58 +16,80 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    char* file = malloc(strlen(argv[1]));
-    strcpy(file, argv[1]);
+    char* link_name = malloc(strlen(argv[1]) + 1);
+    strcpy(link_name, argv[1]);
+
     struct stat sb;
 
-    printf("%s", file);
-
-    char* buf;
-
+    char* link_target = NULL;
+    ssize_t bufsize = 0;
+    short retval = 0;
     do {
-        if(lstat(file, &sb) == -1) {
-            // assume we finished if file doesn't exist
-            // so don't print error
-            // TODO: actually check using path of previous link
-            // if file is in the same directory
-            if(errno != ENOENT)
-                perror("lstat");
-            return 1;
+        if(lstat(link_name, &sb) == -1) {
+            perror("lstat");
+            retval = 1;
+            goto exit;
+        }
+
+        if(link_target == NULL) {
+            printf("%s", link_name);
         }
 
         if(!S_ISLNK(sb.st_mode)) {
             break;
         }
 
-        ssize_t bufsize = sb.st_size != 0 ? sb.st_size + 1 : PATH_MAX;
+        bufsize = sb.st_size != 0 ? sb.st_size + 1 : PATH_MAX;
 
-        buf = malloc(bufsize);
-        if(buf == NULL) {
+        link_target = realloc(link_target, bufsize);
+        if(link_target == NULL) {
             perror("malloc");
-            return 1;
+            retval = 1;
+            goto exit;
         }
 
-        ssize_t nbytes = readlink(file, buf, bufsize);
+        ssize_t nbytes = readlink(link_name, link_target, bufsize);
         if(nbytes == -1) {
             perror("readlink");
-            return 1;
+            retval = 1;
+            goto exit;
         }
 
-        buf[nbytes] = '\0';
+        link_target[nbytes] = '\0';
 
-        printf(" -> %s", buf);
+        printf(" -> %s", link_target);
 
-        if(nbytes == bufsize)
-            printf("(Returned buffer may have been truncated)\n");
+        if(link_target[0] != '/') {
+            char* link_dirname = malloc(strlen(link_name) + 1);
+            strcpy(link_dirname, link_name);
 
-        file = malloc(bufsize);
-        strcpy(file, buf);
-        file[nbytes] = '\0';
+            char* dir = dirname(link_dirname);
+
+            ssize_t full_path_len = strlen(dir) + 1 + strlen(link_target) + 1;
+            char* full_path = malloc(full_path_len);
+            snprintf(full_path, full_path_len, "%s/%s", dir, link_target);
+
+            link_target = realloc(link_target, full_path_len);
+            strcpy(link_target, full_path);
+
+            free(full_path);
+            free(dir);
+        }
+
+        // if(nbytes == bufsize)
+        //     printf("(Returned buffer may have been truncated)\n");
+
+        link_name = realloc(link_name, strlen(link_target) + 1);
+        strcpy(link_name, link_target);
+        link_name[strlen(link_target)] = '\0';
     } while(1);
 
-    free(file);
-    free(buf);
+exit:
+    free(link_name);
+    free(link_target);
 
-    printf("\n");
-    return 0;
+    if(errno == 0) {
+        printf("\n");
+    }
+    return retval;
 }
